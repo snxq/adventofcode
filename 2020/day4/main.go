f7d70224f7e4aea553cd99ed922469fd60c06e9c
snxq <snxq1995@gmail.com>
2021-01-12 00:07:42 +0800

    add 2020/day4/part2

--- 2020/day4/main.go
+++ 2020/day4/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"encoding/hex"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -22,22 +24,140 @@ func main() {
 				end = true
 				break
 			}
-			if len(passport) == 8 {
-				valid++
-				continue
+			filters := []filter{
+				part1, byrFilter, hgtFilter, eyrFilter,
+				hclFilter, eclFilter, pidFilter, iyrFilter,
 			}
-			if _, ok := passport["cid"]; !ok && len(passport) == 7 {
+			if exec(passport, filters) {
 				valid++
 			}
 
 		default:
-			//
 		}
 		if end {
 			break
 		}
 	}
 	fmt.Println("The number of valid passports: ", valid)
+}
+
+func exec(m map[string]string, filters []filter) bool {
+	for _, f := range filters {
+		if !f(m) {
+			return false
+		}
+	}
+	return true
+}
+
+type filter func(map[string]string) bool
+
+func byrFilter(m map[string]string) bool {
+	return durationFilter(m, "byr", 2002, 1920)
+}
+
+func iyrFilter(m map[string]string) bool {
+	return durationFilter(m, "iyr", 2020, 2010)
+}
+
+func eyrFilter(m map[string]string) bool {
+	return durationFilter(m, "eyr", 2030, 2020)
+}
+
+func hgtFilter(m map[string]string) bool {
+	hgt, ok := m["hgt"]
+	if !ok {
+		return false
+	}
+	if len(hgt) <= 2 {
+		return false
+	}
+	number, err := strconv.Atoi(hgt[:len(hgt)-2])
+	if err != nil {
+		return false
+	}
+	switch hgt[len(hgt)-2:] {
+	case "cm":
+		return compare(number, 193, 150)
+	case "in":
+		return compare(number, 76, 59)
+	default:
+		return false
+	}
+}
+
+func hclFilter(m map[string]string) bool {
+	hcl, ok := m["hcl"]
+	if !ok {
+		return false
+	}
+	if !strings.HasPrefix(hcl, "#") {
+		return false
+	}
+	if len(hcl) != 7 {
+		return false
+	}
+	_, err := hex.DecodeString(hcl[1:])
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func eclFilter(m map[string]string) bool {
+	ecl, ok := m["ecl"]
+	if !ok {
+		return false
+	}
+	for _, color := range []string{"amb", "blu", "brn",
+		"gry", "grn", "hzl", "oth"} {
+
+		if color == ecl {
+			return true
+		}
+	}
+	return false
+}
+
+func pidFilter(m map[string]string) bool {
+	pid, ok := m["pid"]
+	if !ok {
+		return false
+	}
+	if len(pid) != 9 {
+		return false
+	}
+	_, err := strconv.Atoi(pid)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func durationFilter(m map[string]string, key string, max, min int) bool {
+	s, ok := m[key]
+	if !ok {
+		return false
+	}
+	v, err := strconv.Atoi(s)
+	if err != nil {
+		return false
+	}
+	return compare(v, max, min)
+}
+
+func compare(current, max, min int) bool {
+	return !(current > max || current < min)
+}
+
+func part1(passport map[string]string) bool {
+	if len(passport) == 8 {
+		return true
+	}
+	if _, ok := passport["cid"]; !ok && len(passport) == 7 {
+		return true
+	}
+	return false
 }
 
 func readData(c chan map[string]string) {
